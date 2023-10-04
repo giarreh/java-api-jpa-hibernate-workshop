9b98c293a677324b2d20b617cc94d814f952c1d5
Dave Ames <david.john.ames@gmail.com>
2023-10-04 16:50:50 +0100

    Updated the Employee example code.

--- src/main/java/com/booleanuk/api/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.booleanuk.api.repository;
-
-import com.booleanuk.api.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
